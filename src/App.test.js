101fe388254fd07a9053497875da99ec9d3c94ad
Ryan <tul09348@temple.edu>
2022-11-02 15:16:03 -0400

    Fixed test cases

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,6 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   
-  const linkElement = screen.getByText(/2022/i);
+  const linkElement = screen.getByText(/Friendlendar/i);
   expect(linkElement).toBeInTheDocument();
 });
