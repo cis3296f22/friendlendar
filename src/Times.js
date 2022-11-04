eb08b1069a61ea84affe1d82dba9e7ecc1f522b2
tul14986 <tul14986@temple.edu>
2022-11-03 20:07:42 -0400

    CSS changes to time selection and event setup message

--- src/Times.js
+++ src/Times.js
@@ -3,7 +3,7 @@ import {useState} from 'react';
 //import Calendar from 'react-calendar';
 import './App.css';
 
-const time = ['01:00','02:00','03:00','04:00','05:00', '06:00', '07:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00',
+const time = ['01:00','02:00','03:00','04:00','05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00',
 '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00', '24:00']
 
 function Times(props) {
@@ -21,12 +21,13 @@ return (
  <div className="times">
    {time.map(times => {
     return (
-    <div>
+    <div class="choices">
       <button onClick={(e)=> displayInfo(e)}> {times} </button>
     </div>
         )
      })}
-    <div>
+    <br></br>
+    <div class="display_message">
       {info ? `Your event has been selected at ${event} ${props.date.toDateString()}` : null}
     </div>
  </div>
