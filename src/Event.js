4e8e17997aec5cfc875b490590ae90fc658ff31e
tul14986 <tul14986@temple.edu>
2022-11-27 19:43:29 -0500

    Added display for title and description

--- src/Event.js
+++ src/Event.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import {CardActions, Typography, CardContent, Button, Card} from "@mui/material"
+import { CardActions, Typography, CardContent, Button, Card } from "@mui/material"
 
 
-const Event = ({event}) => (
+const Event = ({ event }) => (
 
-    <div className='centered'><Card variant="outlined" sx={{minWidth: 275, maxWidth: 500, border: 1, borderColor: 'error.dark' }}>
-        <CardContent>
-            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                Start:
-                <li key={event.start}>{new Date(event.start).toString()}</li>
-            </Typography>
-            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                End:
-                <li key={event.end}>{new Date(event.end).toString()}</li>
-            </Typography>
-        </CardContent>
-    </Card></div>
+  <div className='centered'><Card variant="outlined" sx={{ minWidth: 275, maxWidth: 500, border: 1, borderColor: 'error.dark' }}>
+    <CardContent>
+      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        Title:
+        <div key={event.title}>{event.title}</div>
+      </Typography>
+      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        Description:
+        <div key={event.description}>{event.description}</div>
+      </Typography>
+      
+      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        Start:
+        <div key={event.start}>{new Date(event.start).toString()}</div>
+      </Typography>
+      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        End:
+        <div key={event.end}>{new Date(event.end).toString()}</div>
+      </Typography>
+    </CardContent>
+  </Card></div>
 
 )
 
