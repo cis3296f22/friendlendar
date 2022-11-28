27bc5078b122b9b2e83e2bc5f8c3e247cce1c6b8
tul14986 <tul14986@temple.edu>
2022-11-27 19:27:30 -0500

    added title and description storage functionality into database

--- src/CreateEventModal.js
+++ src/CreateEventModal.js
@@ -6,12 +6,13 @@ import { LocalizationProvider } from '@mui/x-date-pickers';
 import { DateTimePicker} from '@mui/x-date-pickers/DateTimePicker';
 import {post} from './utils.js'
 
+
 function CreateEventModal(props) {
 
     const [startDate, setStartDate] = React.useState(null);
     const [endDate, setEndDate] = React.useState(null);
-
-    
+    const [title, setTitle] = React.useState(null);
+    const [description, setDesc] = React.useState(null);
 
     return(        
         <div>
@@ -22,10 +23,10 @@ function CreateEventModal(props) {
                             <Typography variant='h4' align='center' gutterBottom>
                                 Create Event
                             </Typography>
-                            <TextField id="event-name" label="Event Name" variant="outlined" size='small' />
+                            <TextField id="event-name" value={title} label="Event Name" variant="outlined" size='small' onChange={(event) => {setTitle(event.target.value)}}/>
                         </CardContent>
                         <CardContent>
-                            <TextField id="desc" label="Description" variant="standard" size="large" />
+                            <TextField id="desc" value={description} label="Description" variant="standard" size="large" onChange={(event) => {setDesc(event.target.value)}}/>
                         </CardContent>
                         <CardContent>
                             <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -59,10 +60,15 @@ function CreateEventModal(props) {
                             <Button style={{display:'flex',alignItems:'center',justifyContent:'center'}}onClick={ async () => {props.closeModal(false);
                                 const start = new Date(startDate).getTime();
                                 const end = new Date(endDate).getTime();
-                                console.log(start, end);
+
+                                console.log(start, end, title, description);
                                 
-                                const resolve = await post('/insert', {start, end});
+                                const resolve = await post('/insert', {start, end, title, description});
                                 console.log(resolve);
+                                setDesc(null);
+                                setTitle(null);
+                                setStartDate(null);
+                                setEndDate(null);
                             }} data-cy='submit'>Submit</Button>
                         
                         </CardActions>
